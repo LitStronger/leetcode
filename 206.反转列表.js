a40d89d854b152fd8861f0cc6f8ba632d0f82cc4
LitStronger <1589015065@qq.com>
2021-04-15 00:00:46 +0800

    update

---
+++ 206.反转列表.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    let nodeA = null;
+    let nodeB = head;
+    while (nodeB !== null) {
+        let temp = nodeB.next;
+        nodeB.next = nodeA;
+
+
+        nodeA = nodeB
+        nodeB = temp;
+    }
+    return nodeA
+};
