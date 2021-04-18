07b4e055b41ed88dad21e8ebc5b1e8583be2a380
LitStronger <1589015065@qq.com>
2021-04-18 22:21:08 +0800

    ud

--- 415字符串相加.js
+++ 415字符串相加.js
@@ -31,4 +31,30 @@ var addStrings = function (num1, num2) {
     }
     return res.split('').reverse().join('');
 };
-console.log(addStrings("77", "456"))
+// console.log(addStrings("77", "456"))
+
+
+// 结构优化
+function addStrings2(num1, num2) {
+    let len1 = num1.length;
+    let len2 = num2.length;
+    let res = '';
+    let flag = 0;
+    while (len1 > 0 || len2 > 0) {
+        let sum = 0;
+        if (len1 > 0 && len2 > 0)
+            sum = parseInt(num1[len1 - 1]) + parseInt(num2[len2 - 1]) + flag;
+        else if (len1 > 0)
+            sum = parseInt(num1[len1 - 1]) + flag;
+        else if (len2 > 0)
+            sum = parseInt(num2[len2 - 1]) + flag;
+        flag = parseInt(sum / 10)
+        res += sum % 10;
+        len1--;
+        len2--;
+    }
+    if (flag > 0)
+        res += flag;
+    return res.split('').reverse().join('')
+}
+console.log(addStrings2("0", ""))
