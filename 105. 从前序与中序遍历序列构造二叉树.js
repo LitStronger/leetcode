c12599ccf0adf08ed6ac845c69efccceced4c73e
LitStronger <1589015065@qq.com>
2021-06-20 10:23:37 +0800

    6.20

---
+++ 105. 从前序与中序遍历序列构造二叉树.js
@@ -0,0 +1,21 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+const preorder = [3, 9, 20, 15, 7]
+const inorder = [9, 3, 15, 20, 7]
+
+function TreeBuild(preorder, inorder) {
+    if (!preorder.length) return null
+    const root = new TreeNode();
+    root.val = preorder[0];
+    const midIndex = inorder.findIndex(e => e === preorder[0])
+    root.left = TreeBuild(preorder.slice(1, midIndex+1) ,inorder.slice(0, midIndex))
+    root.right = TreeBuild(preorder.slice(midIndex+1) ,inorder.slice(midIndex+1))
+    return root
+}
+
+console.log(JSON.stringify(TreeBuild(preorder, inorder)))
+console.log(TreeBuild(preorder, inorder))
